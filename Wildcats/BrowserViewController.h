1723f42337ef5093fa6746e12917427d8ab8769a
Piet <piet@nerdishbynature.com>
2012-09-20 20:49:40 +0200

    added browser viewcontroller

---
+++ Wildcats/BrowserViewController.h
@@ -0,0 +1,13 @@
+//
+//  BrowserViewController.h
+//  wildcats
+//
+//  Created by Piet Brauer on 20.09.12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BrowserViewController : UIViewController
+
+@end
---
+++ Wildcats/BrowserViewController.m
@@ -0,0 +1,38 @@
+//
+//  BrowserViewController.m
+//  wildcats
+//
+//  Created by Piet Brauer on 20.09.12.
+//
+//
+
+#import "BrowserViewController.h"
+
+@interface BrowserViewController ()
+
+@end
+
+@implementation BrowserViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
